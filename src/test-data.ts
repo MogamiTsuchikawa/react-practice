3de37b18e11c273dd02e0535bd5ce18891f368ea
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2022-01-15 17:30:42 +0900

    テスト用データを追加

---
+++ src/test-data.ts
@@ -0,0 +1,12 @@
+import { Book } from "./interface";
+
+export const books: Book[] = [
+  {
+    name: "TypeScript入門",
+    value: 1240
+  },
+  {
+    name: "EffectivePython",
+    value: 3960
+  }
+]
